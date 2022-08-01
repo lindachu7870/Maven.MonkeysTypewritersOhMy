83e18571ee94f7fed88025dca1969f532a59affa
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-01 10:29:18 -0400

    unsafecopier done

--- src/main/java/io/zipcoder/UnsafeCopier.java
+++ src/main/java/io/zipcoder/UnsafeCopier.java
@@ -1,14 +1,32 @@
 package io.zipcoder;
 
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.sleep;
+
 /**
  * Modify the run function so that the monkeys each grab the next word and write it to the copy.
  */
 public class UnsafeCopier extends Copier {
+    private boolean _debug = true;
 
     public UnsafeCopier(String toCopy) {
         super(toCopy);
     }
 
     public void run() {
+
+        while (stringIterator.hasNext()) {
+            try {
+
+                sleep(600);
+                if (stringIterator.hasNext()) {
+                    copied = copied + stringIterator.next() + " ";
+                    if (_debug) System.out.println(currentThread().getName());
+                }
+
+                } catch (InterruptedException e) {
+                e.printStackTrace();
+             }
+        }
     }
 }
